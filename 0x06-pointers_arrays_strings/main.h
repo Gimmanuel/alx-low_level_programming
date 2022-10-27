5694a197d7cb703644b7a412b5eefbb24f81d5d4
Gmmanuel <gimwalimanuel2019@gmail.com>
2022-10-27 06:25:35 -0700

    task 0

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -16,6 +15,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c)
 	
-
 #endif
 
